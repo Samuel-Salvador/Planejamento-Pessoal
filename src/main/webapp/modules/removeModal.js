c3d36389faa51273fe76ca24ab1a0b1a346b4530
SamuelSalvador <samuel20018@gmail.com>
2025-04-12 21:21:28 -0300

    fixed url http delete

--- src/main/webapp/modules/removeModal.js
+++ src/main/webapp/modules/removeModal.js
@@ -1,6 +1,6 @@
 import {transactionsArray,setTotal} from "./finance.js";
-import {formattedDate,formattedPrice,userClickEvents } from "./global.js";
-import { transactionUrl,userUrl } from "./login.js";
+import {formattedDate,formattedPrice,userClickEvents,url } from "./global.js";
+import { userUrl } from "./login.js";
 import { setUpChart } from "./categoryChart.js";
 import { updateBalanceHeader,userData } from "./header.js";
 import { changePlaceholdersUserData } from "./settingsModal.js";
@@ -103,7 +103,7 @@ export function initRemovalModal(){
 		//http DELETE
 		removalButtonConfirm.addEventListener(userEvent,(event)=>{
 				
-				fetch(transactionUrl.substring(0,transactionUrl.length-2)+"/"+removalIdDB,{method: "DELETE"})
+				fetch(url+`transactions/`+removalIdDB,{method: "DELETE"})
 					.then(removeFromDOMSelectedTransaction());
 				closeRemovalModal(event);
 				
