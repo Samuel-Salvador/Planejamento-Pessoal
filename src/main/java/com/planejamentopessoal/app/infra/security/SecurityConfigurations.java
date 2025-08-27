fddad12770cdb170693c861a80cd9d8141d4cace
Samuel S <samuel20018@gmail.com>
2025-08-27 16:05:24 -0300

    permiting all http req from /registers

--- src/main/java/com/planejamentopessoal/app/infra/security/SecurityConfigurations.java
+++ src/main/java/com/planejamentopessoal/app/infra/security/SecurityConfigurations.java
@@ -36,6 +36,7 @@ public class SecurityConfigurations {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/login").permitAll();
+                    req.requestMatchers("/register").permitAll();
                     req.requestMatchers(HttpMethod.POST,"/users").permitAll();
                     req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated();
